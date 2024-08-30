4265f967ac4e151215bf313f1ae41ddb63597f0f
Matej Mohorcic <mohori.matej@gmail.com>
2024-08-30 18:43:37 +0200

    fix

--- test.cmd
+++
@@ -1,7 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'test.exe' with Python DLL being found.
-set PATH=C:\Users\mohor\AppData\Local\Programs\Python\Python39;%PATH%
-set PYTHONHOME=C:\Users\mohor\AppData\Local\Programs\Python\Python39
-set NUITKA_PYTHONPATH=C:\Users\mohor\Documents\1_PROJEKTI\1_odprti\truckSpace\test;C:\Users\mohor\AppData\Local\Programs\Python\Python39\DLLs;C:\Users\mohor\AppData\Local\Programs\Python\Python39\lib;C:\Users\mohor\AppData\Local\Programs\Python\Python39;C:\Users\mohor\AppData\Local\Programs\Python\Python39\lib\site-packages
-"%~dp0test.exe" %*
