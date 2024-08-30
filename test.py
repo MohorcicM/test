26f62c2bd0d1998aee47436f7a67b6b8ee82f6f3
Matej Mohorcic <mohori.matej@gmail.com>
2024-08-30 17:53:18 +0200

    Upload test script

---
+++ test.py
@@ -0,0 +1,13 @@
+# Get git version
+import subprocess
+import os
+
+# get git tag
+def get_git_version():
+    try:
+        git_version = subprocess.check_output(["git", "describe", "--tags", "--always"], stderr=subprocess.STDOUT).strip().decode('utf-8')
+    except subprocess.CalledProcessError:
+        git_version = "unknown"
+    return git_version
+    
+print("Git version: %s" % get_git_version())
